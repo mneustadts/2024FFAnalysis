532be2378c687ad4e7cb5bbbf7a68e7ed8e171b6
Michael Neustadter <mneustadts@gmail.com>
2025-01-17 09:29:32 -0600

    Added a consensus position rank field

--- Consolidated_ADP.sql
+++ Consolidated_ADP.sql
@@ -1,18 +1,3 @@
--- Create a consolidated ADP table for each site
-SELECT 
-    e.Player AS Player,
-    e.ADP AS ESPN_ADP,
-    n.ADP AS NFL_ADP,
-    r.ADP AS ROTO_ADP,
-    (e.ADP + n.ADP + r.ADP) / 3.0 AS Cons_ADP
-FROM 
-    ESPN_Data e
-LEFT JOIN 
-    NFL_Data n ON e.Player = n.Player
-LEFT JOIN 
-    Roto_Data r ON e.Player = r.Player;
-
--- Now create a Position rankings table to get the variance of	
 WITH ESPN_Rankings AS (
     SELECT 
         Player,
@@ -30,21 +15,8 @@ Roto_Rankings AS (
         Player,
         Position || CAST(ROW_NUMBER() OVER (PARTITION BY Position ORDER BY ADP) AS TEXT) AS ROTO_PosRank
     FROM Roto_Data
-)
-SELECT 
-    e.Player AS Player,
-    e.ESPN_PosRank,
-    n.NFL_PosRank,
-    r.ROTO_PosRank
-FROM 
-    ESPN_Rankings e
-LEFT JOIN 
-    NFL_Rankings n ON e.Player = n.Player
-LEFT JOIN 
-    Roto_Rankings r ON e.Player = r.Player;
-
--- Finally create a table for the actual rankings from the acutal stat table	
-WITH PositionInfo AS (
+),
+PositionInfo AS (
     SELECT 
         f.Player AS Player,
         p.Position AS Position,
@@ -62,11 +34,50 @@ RankedPositions AS (
         Position || CAST(ROW_NUMBER() OVER (PARTITION BY Position ORDER BY PPRpts DESC) AS TEXT) AS Act_Rankings
     FROM 
         PositionInfo
+),
+Consolidated_ADP AS (
+    SELECT 
+        e.Player AS Player,
+        e.ADP AS ESPN_ADP,
+        n.ADP AS NFL_ADP,
+        r.ADP AS ROTO_ADP,
+        (e.ADP + n.ADP + r.ADP) / 3.0 AS Consensus_ADP
+    FROM 
+        ESPN_Data e
+    LEFT JOIN 
+        NFL_Data n ON e.Player = n.Player
+    LEFT JOIN 
+        Roto_Data r ON e.Player = r.Player
+),
+Consensus_Rankings AS (
+    SELECT 
+        c.Player AS Player,
+        p.Position || CAST(ROW_NUMBER() OVER (PARTITION BY p.Position ORDER BY c.Consensus_ADP) AS TEXT) AS Consensus_PosRank
+    FROM 
+        Consolidated_ADP c
+    JOIN 
+        PlayerInfo p ON c.Player = p.Player
 )
 SELECT 
-    Player,
-    Position,
-    PPRpts,
-    Act_Rankings
+    c.Player AS Player,
+    c.ESPN_ADP,
+    c.NFL_ADP,
+    c.ROTO_ADP,
+    c.Consensus_ADP,
+    er.ESPN_PosRank,
+    nr.NFL_PosRank,
+    rr.ROTO_PosRank,
+    cr.Consensus_PosRank,
+    rp.Act_Rankings
 FROM 
-    RankedPositions;
+    Consolidated_ADP c
+LEFT JOIN 
+    ESPN_Rankings er ON c.Player = er.Player
+LEFT JOIN 
+    NFL_Rankings nr ON c.Player = nr.Player
+LEFT JOIN 
+    Roto_Rankings rr ON c.Player = rr.Player
+LEFT JOIN 
+    RankedPositions rp ON c.Player = rp.Player
+LEFT JOIN 
+    Consensus_Rankings cr ON c.Player = cr.Player;
